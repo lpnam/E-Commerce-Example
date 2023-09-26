d6806a5a75359096122414879ff01e4d4579d272
lpnam <kgzwhite00@gmail.com>
2023-09-26 20:45:20 +0700

    full-code

--- src/App.js
+++ src/App.js
@@ -1,49 +1,46 @@
-import { 
-  createBrowserRouter,
-  RouterProvider,
-  Outlet
- } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Product from './pages/Product/Product';
-import Products from './pages/Products/Products';
-import Navbar from './component/Navbar/Navbar';
-import Footer from './component/Footer/Footer';
-import './app.scss';
+import { Children } from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import "./app.scss"
 
 const Layout = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 const router = createBrowserRouter([
-{
-  path:"/",
-  element: <Layout/>,
-  children:[
-    {
-      path:"/",
-      element: <Home/>
-    },
-    {
-      path:"/products/:id",
-      element: <Products/>
-    },
-    {
-      path:"/product/:id",
-      element: <Product/>
-    },
-  ]
-},
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
 ]);
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={router} />
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import './Home.scss';
-import Slider from '../../component/Slider/Slider'
-import FeaturedProducts from '../../component/FeaturedProducts/FeaturedProducts';
-import Categories from '../../component/Categories/Categories';
-import Contact from '../../component/Contact/Contact';
-
+import React from 'react'
+import Categories from '../../components/Categories/Categories'
+import Contact from '../../components/Contact/Contact'
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
+import Slider from '../../components/Slider/Slider'
+import "./Home.scss"
 const Home = () => {
   return (
     <div className='home'>
